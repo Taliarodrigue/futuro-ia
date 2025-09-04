53cd2713c9f69ddc476cd4421fba3c9012638317
Taliarodrigue <166857934+Taliarodrigue@users.noreply.github.com>
2025-09-04 17:18:19 -0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+const caixaPrincipal = document.querySelector(".caixa-principal")
+const caixaPerguntas = document.querySelector(".caixas-perguntas");
+const caixasAlternativas = document.querySelector(".caicas-alternativas");
+  
